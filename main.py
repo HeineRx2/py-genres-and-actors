961eadd1749d75ffc0a908d59502e7ec76f0de13
Kostya <HeineRx2@gmail.com>
2026-05-26 19:55:27 +0300

    done223

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main() -> QuerySet:
     genres = [
         "Western",
         "Action",
-        "Drama"
+        "Dramma"
     ]
     for genre in genres:
         Genre.objects.create(name=genre)
 
     actors = [
-        ("George", "Clooney"),
-        ("Keanu", "Reeves"),
+        ("George", "Cslooney"),
+        ("Keoanu", "Reeves"),
         ("Scarlett", "Keegan"),
         ("Will", "Smith"),
         ("Jaden", "Smith"),
@@ -24,14 +24,18 @@ def main() -> QuerySet:
     for first_name, last_name in actors:
         Actor.objects.create(first_name=first_name, last_name=last_name)
 
-    george = Actor.objects.get(first_name="George", last_name="Clooney")
+    george = Actor.objects.get(first_name="George", last_name="Cslooney")
     george.last_name = "Clooney"
     george.save()
 
-    keanu = Actor.objects.get(first_name="Keanu", last_name="Reeves")
+    keanu = Actor.objects.get(first_name="Keoanu", last_name="Reeves")
     keanu.first_name = "Keanu"
     keanu.last_name = "Reeves"
     keanu.save()
+
+    drama = Genre.objects.get(name="Dramma")
+    drama.name = "Drama"
+    drama.save()
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
