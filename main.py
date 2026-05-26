8c563b85fdfde8c15c81322eb038213a073060a5
Kostya <HeineRx2@gmail.com>
2026-05-26 19:16:48 +0300

    done

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    pass
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Drama")
+
+    Actor.objects.create(first_name="George", last_name="Clooney")
+    Actor.objects.create(first_name="Keanu", last_name="Reeves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    george = Actor.objects.get(first_name="George", last_name="Clooney")
+    george.last_name = "Clooney"
+    george.save()
+
+    keanu = Actor.objects.get(first_name="Keanu", last_name="Reeves")
+    keanu.first_name = "Keanu"
+    keanu.last_name = "Reeves"
+    keanu.save()
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
